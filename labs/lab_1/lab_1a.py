7afddd7c232f89c60241904f5bc91dc6ec26a9ad
Aarin <aarin.sandilya12@gmail.com>
2026-03-28 17:08:47 -0500

    goated

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Aarin" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Im goated")
 
 if __name__ == "__main__":
     main()
